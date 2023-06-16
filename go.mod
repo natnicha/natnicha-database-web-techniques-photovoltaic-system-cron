0bc9bb6c2e347b49161545f745f07a36e8b351e4
Natnicha <nat.rodtong@gmail.com>
2023-06-16 12:05:39 +0200

    feat: fix schedule time for cron job

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module photovoltaic-system-cron
 
 go 1.20
 
-require github.com/robfig/cron/v3 v3.0.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/robfig/cron/v3 v3.0.0
+)
--- main.go
+++ main.go
@@ -7,27 +7,29 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/joho/godotenv"
 	"github.com/robfig/cron/v3"
 )
 
 func main() {
 	c := cron.New()
-	c.AddFunc("* * * * * *", func() { callDailyWeather() })
-	go c.Start()
+	c.AddFunc("5 0 * * *", callDailyWeather) //every 0.05 AM
+	c.Start()
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt, os.Kill)
 	<-sig
 }
 
 func callDailyWeather() {
-	request, err := http.Get("http://localhost:" + os.Getenv("SERVICE_PORT") + "/weather/daily")
+	godotenv.Load(".env")
+	request, err := http.Post("http://localhost:"+os.Getenv("SERVICE_PORT")+"/weather/daily", "", nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
-	if request.StatusCode != 200 {
+	if request.StatusCode != http.StatusAccepted {
 		err := errors.New("Requesting to daily weather failed")
-		log.Fatal(err)
+		log.Println(err.Error())
 		return
 	}
 }
